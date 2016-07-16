33d594c5b960414b00e9923568fdff8226695f1e
sleepyUdon <vivianechan@hotmail.com>
2016-07-16 13:08:33 -0400

    first commit

---
+++ Insta-Kilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Insta-Kilo
+//
+//  Created by Viviane Chan on 2016-07-16.
+//  Copyright © 2016 LightHouse Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Insta-Kilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Insta-Kilo
+//
+//  Created by Viviane Chan on 2016-07-16.
+//  Copyright © 2016 LightHouse Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
